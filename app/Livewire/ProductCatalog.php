8b7e1112df2485d213f834296ab0d63490ee1d34
AlvinSanudharma <alvinsanudharma@gmail.com>
2025-08-02 09:55:54 +0800

    Passing Data To View (Basic)

--- app/Livewire/ProductCatalog.php
+++ app/Livewire/ProductCatalog.php
@@ -2,12 +2,17 @@
 
 namespace App\Livewire;
 
+use App\Models\Product;
 use Livewire\Component;
 
 class ProductCatalog extends Component
 {
     public function render()
     {
-        return view('livewire.product-catalog');
+        $products = Product::paginate(9);
+
+        // TODO Make a DTO
+
+        return view('livewire.product-catalog', compact('products'));
     }
 }
