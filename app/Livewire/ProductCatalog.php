cf3215863fdc6ba238836c68f8056c2fdee88bf0
AlvinSanudharma <alvinsanudharma@gmail.com>
2025-08-04 15:54:40 +0800

    Product Collection Data

--- app/Livewire/ProductCatalog.php
+++ app/Livewire/ProductCatalog.php
@@ -4,18 +4,22 @@ declare(strict_types=1);
 
 namespace App\Livewire;
 
+use App\Data\ProductCollectionData;
 use App\Data\ProductData;
 use App\Models\Product;
+use App\Models\Tag;
 use Livewire\Component;
 
 class ProductCatalog extends Component
 {
     public function render()
     {
+        $collection_result = Tag::query()->withType('collection')->withCount('products')->get();
         $result = Product::paginate(9);
 
         $products = ProductData::collect($result);
+        $collections = ProductCollectionData::collect($collection_result);
 
-        return view('livewire.product-catalog', compact('products'));
+        return view('livewire.product-catalog', compact('products', 'collections'));
     }
 }
