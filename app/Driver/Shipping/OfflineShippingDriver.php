4bca2a3929327d5a8737233d6d13674e67351641
AlvinSanudharma <alvinsanudharma@gmail.com>
2025-09-04 20:28:14 +0800

    Preparing Shipping Part 1

---
+++ app/Driver/Shipping/OfflineShippingDriver.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Driver\Shipping;
+
+use App\Contract\ShippingDriverInterface;
+
+class OfflineShippingDriver implements ShippingDriverInterface { }
