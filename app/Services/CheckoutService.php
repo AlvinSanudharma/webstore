0c41b670c3d8fcc95f9e2611d0677f56c7a1a556
AlvinSanudharma <alvinsanudharma@gmail.com>
2025-09-15 12:55:03 +0800

    Build Sales Order Model & Migration

--- app/Services/CheckoutService.php
+++ app/Services/CheckoutService.php
@@ -6,9 +6,12 @@ namespace App\Services;
 
 use App\Data\CheckoutData;
 use App\Data\SalesOrderData;
+use Illuminate\Support\Facades\DB;
 
 class CheckoutService {
-    public function makeAnOrder(CheckoutData $checkout_data): SalesOrderData {
-        return new SalesOrderData();
-    }
+    // public function makeAnOrder(CheckoutData $checkout_data): SalesOrderData {
+    //     DB::transaction(function() use($checkout_data) {
+    //         SalesOrder
+    //     });
+    // }
 }
